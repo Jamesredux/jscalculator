cb7e0f28b4f31f749eb65a6d358764163af2268b
James McCann <jamesredux@gmail.com>
2020-02-11 20:09:56 +0700

    calculator functionig

---
+++ scripts/script.js
@@ -0,0 +1,274 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+	init();
+}, false);
+
+let equation = ['0'];
+let firstObjectIsResult = false;
+
+function lastObject(array) {
+	return array[array.length-1];
+}
+
+
+
+
+
+function init() {
+
+	const numButtons = document.getElementsByClassName("number-button");
+	const operatorButtons = document.getElementsByClassName("operator-button");
+	const decimalButton = document.querySelector('.decimal-button');
+	const equalsButton = document.querySelector('.equals-button');
+	const clearButton = document.querySelector('.clear-button');
+	const negativeButton = document.querySelector('.negative-button');
+ 
+	keyDownSetUp();
+
+
+
+	for (i  =  0; i < numButtons.length; i++) {
+		numButtons[i].addEventListener('click', (e) => {
+				addNumber(e.target.textContent);
+		});
+		
+	};
+	
+
+
+	for (i  =  0; i < operatorButtons.length; i++) {
+		operatorButtons[i].addEventListener('click', (e) => {
+			addOperator(e.target.innerText);
+		 //function to add operator if ok to display and equation.
+		});
+	};
+
+	decimalButton.addEventListener("click", (e) => {
+			addDecimal(e.target.textContent);
+	});
+
+	equalsButton.addEventListener('click', (e) => {
+			runCalculation();
+	});
+
+	clearButton.addEventListener('click', (e) => {
+			allClear();
+	});
+
+	negativeButton.addEventListener('click', (e) => {
+			toggleNegative();
+	});
+
+	updateDisplay(equation);
+
+};
+
+let sum = (a, b) => Math.floor((a + b)*100) / 100;
+
+let multiply = (a, b) => Math.floor((a*b)*100) / 100;
+
+let minus = (a, b) => Math.floor((a-b)*100) / 100;
+
+let divide = (a, b) => Math.floor((a/b)*100) / 100;
+
+const testEq = [12, "+", 12, "*", 3, '/', 2, "-", 4];
+
+const testEq2 = [12, "-", 12, "*", 3, '+', 12, "/", 4];
+
+
+function calculateEquation(array) {
+	convertNumbers(array);
+	if (array.length < 2) {
+
+		equation = array.map(x => x.toString()); //converts array back to strings
+		firstObjectIsResult = true;
+
+	} else {
+
+		array.forEach(evaluateEquation); //  first pass should evaluate * and minus /
+		let newArray = array.filter(function(e){return e!==null; }); // removes nulls
+		calculateEquation(newArray); // call function again to evaluate + and -
+	};
+};
+
+function evaluateEquation(item, index, array) {
+
+	if (isNaN(item)) {
+		if (item == "*" || item == '/') {
+			let multiProduct = doCalculation(array[index-1], item, array[index+1]);
+			array[index+1] = multiProduct;
+			array[index] = null;
+			array[index-1] = null;
+			
+
+		} else if (!array.includes("*") && !array.includes("/"))  {
+			let sumProduct = doCalculation(array[index-1], item, array[index+1]);
+			array[index+1] = sumProduct; // replace one of array object with result of sum
+			array[index] = null; // remove the other 2 objects - turn them to null they will be filtered by calculateEquation function
+			array[index-1] = null;
+
+
+		};
+	}
+};
+
+function doCalculation(a, operator, b ) {
+	let result = 0;
+	switch(operator) {
+		case '*':
+		result = multiply(a, b);
+		break
+		case  '/':
+		result = divide(a, b);
+		break
+		case '+':
+		result = sum(a, b);
+		break
+		case '-':
+		result = minus(a, b);
+		break
+	};
+	return result;
+
+};
+
+
+	function updateDisplay(equation) {
+		const displayScreen = document.querySelector('.screen');
+		screenOutput = equation.join(" ")
+		displayScreen.textContent = screenOutput;
+	}
+
+
+	function addNumber(number) {
+		if (isNaN(equation[equation.length-1]/1)) {
+			equation.push(number);
+		}
+		else if( equation[equation.length-1] == '0' || firstObjectIsResult == true) {
+			equation[equation.length-1] = number;
+		}
+		else {
+			equation[equation.length-1] += number
+		}
+		firstObjectIsResult = false;
+		updateDisplay(equation);
+	};
+
+	function addDecimal(point) {
+		//doesn't work for 0
+		if (firstObjectIsResult == true) {
+			equation[equation.length-1] = '0.';
+		} 
+		if(!isNaN(equation[equation.length-1]) && !equation[equation.length-1].includes(point)) {
+			equation[equation.length-1] += point
+		}
+		firstObjectIsResult = false;
+		updateDisplay(equation);
+	}
+
+	function addOperator(operator) {
+	if (!isNaN(equation[equation.length-1])) {
+		equation.push(operator);
+	}
+	firstObjectIsResult = false;
+	updateDisplay(equation);
+	}
+
+	function runCalculation() {
+
+		calculateEquation(equation);
+		updateDisplay(equation);
+	}
+
+
+	function convertNumbers(array) {
+			array.forEach(function(object, index) {		
+				if (!isNaN(object)) {
+					array[index] = parseFloat(object);			
+			}; 	
+		});
+		if (isNaN(lastObject(array))) {
+			array.pop();  // if user leaves operator as last element this removes it before calculation eg 5 + 6 + =
+		};	
+	};
+
+	function allClear() {
+		equation = [0];
+		updateDisplay(equation);
+	};
+
+	function toggleNegative() {
+		x = lastObject(equation)
+		if (!isNaN(x)) {
+			console.log(x);
+			x.indexOf("-") == 0 ? x = x.replace("-", "") : x = "-" + x;
+			equation[equation.length-1] =  x;
+			//equation[equation.length-1] *= -1;
+			updateDisplay(equation);
+		};
+	};
+	//to do what to do if operator then equals pressed
+	//what to do with result
+	//ac
+
+	//function numberPressed(code) {
+	//	switch (code) {
+	//		case: 
+	//	}
+	//}
+
+	function keyDownSetUp() {
+		document.addEventListener('keydown', e => {
+  switch (e.key) {
+    case '+':
+      //document.querySelector(`.badd`).classList.add('active')
+      addOperator(e.key);
+      break;
+    case '-':
+      //document.querySelector(`.bsubtract`).classList.add('active')
+      addOperator(e.key);
+      break;
+    case '*':
+     // document.querySelector(`.bmultiply`).classList.add('active')
+      addOperator(e.key);
+      break;
+    case '/':
+      //document.querySelector(`.bdivide`).classList.add('active')
+      addOperator(e.key);
+      break;
+    case '.':
+     // document.querySelector(`.bdecimal`).classList.add('active')
+      addDecimal(e.key);
+      break;
+    case 'Enter':
+    case '=':
+      //document.querySelector(`.bequals`).classList.add('active')
+      runCalculation();
+      break;
+    case 'Delete':
+    	allClear();
+    	break;	
+    	  
+   // case 'Backspace':
+     //// document.querySelector(`.bAC`).classList.add('active')
+     // backspace();
+      //break;
+   // case 'Delete':
+     // document.querySelector(`.bC`).classList.add('active')
+    //  clear();
+     // break;
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+     // document.querySelector(`.b${e.key}`).classList.add('active')
+      addNumber(e.key);
+  }
+});
+	}
